599470d684c279e855cc8082be4b82f1a4cd6ff1
Saurabh Bhalla <saurabh.bhalla90@gmail.com>
2020-07-15 00:23:39 -0500

    modified 1.2

--- ElementsOfProgrammingInterview/01_PrimitiveTypes/02_SwapBits.cs
+++ ElementsOfProgrammingInterview/01_PrimitiveTypes/02_SwapBits.cs
@@ -3,7 +3,7 @@ using System;
 public class Solution {
     public static long SwapBits(long x, int i, int j) {
         if((x>>i) & 1 != (x>>j) & 1) {
-            x ^= (1<<i) | (1<<j);
+            x ^= (1L<<i) | (1L<<j);
         }
         return x;
     }
